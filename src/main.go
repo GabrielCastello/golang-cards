2dcf21ec09454411152d599963fdbdda1cf4426e
Gabriel Castello <gabriel__castello@hotmail.com>
2020-02-13 14:06:41 -0300

    removed deck creation from main.go and replaced the deck creation with newDeck() from deck.go

--- src/main.go
+++ src/main.go
@@ -1,32 +1,8 @@
 package main
 
-import (
-	"fmt"
-	// "log"
-	// "net/http"
-)
-
 func main() {
-
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	// card := newCard()
-
-	// fmt.Println(card)
-	fmt.Println(cards)
+	cards := newDeck()
 
 	cards.print()
 
-	// fmt.Println("Go Docker Tutorial!!!!")
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello World !!!")
-	// })
-
-	// log.Fatal(http.ListenAndServe(":8081", nil))
-
-}
-
-func newCard() string {
-	return "Fice of Diamonds"
 }
